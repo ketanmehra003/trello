187cc771ee23540553c7538c5b1f48abc793a3e2
Ashu Patel <ashutoshpatel2702@gmail.com>
2024-07-31 01:27:46 +0530

    Added Auth routes and frontend

---
+++ src/app/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('token');
+
+  if (!token) {
+    return NextResponse.next();
+  }
+
+  return NextResponse.redirect(new URL('/dashboard', request.url));
+}
+
+export const config = {
+  matcher: '/',
+};
